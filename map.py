887a80de136e023404c6b3380fcd50ecec7ac579
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-02-05 22:31:57 +0000

    generator command called

--- map.py
+++ map.py
@@ -3,8 +3,12 @@
 """
 Module to work with world maps (Generation, edition, save...)
 """
+from subprocess import call
+
+import config
 
 class map:
 	@staticmethod
 	def generate(name, width, height):
-		print name, width, height
+		command = config.generator['map']['generator'] % (config.generator['map']['destination-dir'] + '/' + name, width, height)
+		call(command, shell=True)
