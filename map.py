10d5a49281a95a8dd7ff25998abd1d9bcb4a7dcf
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-02-05 22:49:52 +0000

    indentation

--- map.py
+++ map.py
@@ -10,5 +10,9 @@ import config
 class map:
 	@staticmethod
 	def generate(name, width, height):
-		command = config.generator['map']['generator'] % (config.generator['map']['destination-dir'] + '/' + name, width, height)
+		command = config.generator['map']['generator'] % (
+			config.generator['map']['destination-dir'] + '/' + name,
+			width,
+			height
+		)
 		subprocess.call(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
