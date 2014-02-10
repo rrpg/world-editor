a84fc85b619969994469d4211806af4b24fe9179
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-02-10 13:34:39 +0000

    list species GUI added

--- gui/specieslistdialog.py
+++ gui/specieslistdialog.py
@@ -1,7 +1,6 @@
 # -*- coding: utf8 -*-
 
-from PyQt4 import QtGui
-
+from PyQt4 import QtGui, QtCore
 
 class speciesListDialog(QtGui.QDialog):
 
@@ -16,4 +15,60 @@ class speciesListDialog(QtGui.QDialog):
 		self.show()
 
 	def initUI(self):
-		pass
+		layout = QtGui.QVBoxLayout(self)
+
+		tablemodel = SpeciesTableModel(self._app.map.species, self)
+		tableview = QtGui.QTableView()
+		tableview.setModel(tablemodel)
+
+		form = QtGui.QGridLayout()
+
+		nameLabel = QtGui.QLabel("Species name")
+		self._nameField = QtGui.QLineEdit()
+		descriptionLabel = QtGui.QLabel("Species Description")
+		self._descriptionField = QtGui.QTextEdit()
+
+		self._saveButton = QtGui.QPushButton("Create")
+		self._saveButton.clicked.connect(self.createSpecies)
+
+		form.addWidget(nameLabel, 0, 0)
+		form.addWidget(self._nameField, 0, 1)
+		form.addWidget(descriptionLabel, 1, 0)
+		form.addWidget(self._descriptionField, 1, 1)
+		form.addWidget(self._saveButton, 2, 1)
+
+		layout.addWidget(tableview)
+		layout.addLayout(form)
+		self.setLayout(layout)
+
+	def createSpecies(self):
+		print self._nameField.text(), self._descriptionField.toPlainText()
+
+
+class SpeciesTableModel(QtCore.QAbstractTableModel):
+	def __init__(self, datain, parent = None, *args):
+		QtCore.QAbstractTableModel.__init__(self, parent, *args)
+		self.dataChanged.connect(self.saveChange)
+		self.arraydata = datain
+
+	def rowCount(self, parent):
+		return len(self.arraydata)
+
+	def columnCount(self, parent):
+		if len(self.arraydata) == 0:
+			return 0
+
+		return len(self.arraydata[0])
+
+	def data(self, index, role):
+		if not index.isValid():
+			return None
+		elif role != QtCore.Qt.DisplayRole:
+			return None
+		return (self.arraydata[index.row()][index.column()])
+
+	def saveChange(self, x, y):
+		print x, y
+
+	def flags(self, index):
+		return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
