22f2a6edb6a7bb10598e2eee3c913449df745e4b
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-02-11 21:55:43 +0000

    comments added in the intLineEdit class

--- gui/intlineedit.py
+++ gui/intlineedit.py
@@ -4,7 +4,14 @@ from PyQt4 import QtGui
 
 
 class intLineEdit(QtGui.QLineEdit):
+	"""
+	Overload of the QLineEdit widget to have only integer values.
+	Should be replaced with a spinbox widget
+	"""
 	def value(self):
+		"""
+		The value must always return an integer
+		"""
 		return int(self.text())
 
 
