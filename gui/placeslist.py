2842fb85b75ca26c13f1bd57abbf150b6c22de27
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-02-21 13:31:47 +0000

    import fixed

--- gui/placeslist.py
+++ gui/placeslist.py
@@ -2,10 +2,10 @@
 
 from PyQt4 import QtGui
 from PyQt4 import QtCore
-from gui.itemlist import itemList
+import gui.itemlist
 
 
-class placesList(itemList):
+class placesList(gui.itemlist.itemList):
 
 	_columns = ('Name', 'Type', 'X', 'Y', 'Locate')
 
@@ -14,7 +14,7 @@ class placesList(itemList):
 		self.setItem(index, 1, QtGui.QTableWidgetItem(self._app.map.getPlaceTypesLabels()[row['type']]))
 		self.setItem(index, 2, QtGui.QTableWidgetItem(str(row['coordinates'][0])))
 		self.setItem(index, 3, QtGui.QTableWidgetItem(str(row['coordinates'][1])))
-		self.setCellWidget(index, 4, placeLocatorButton(self, index, "Locate"))
+		self.setCellWidget(index, 4, gui.itemlist.itemLocatorButton(self, index, "Locate"))
 		self.resizeColumnsToContents()
 
 	def getData(self):
