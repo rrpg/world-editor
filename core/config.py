42730cc59d91d77447cd939f7fac66e3d7e52c06
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-03-04 21:38:02 +0000

    the brush must be null, not the pen

--- core/config.py
+++ core/config.py
@@ -22,7 +22,7 @@ generator['map'] = {
 db = exportPath + '/%s.db'
 
 colors = {}
-colors['selected-cell'] = [(0, 0, 0), None]
+colors['selected-cell'] = [None, (0, 0, 0)]
 colors['start-cell'] = [(0, 0, 0), (0, 0, 0)]
 colors['place'] = [(127, 127, 127), (127, 127, 127)]
 colors['npc'] = [(127, 127, 127), (127, 127, 127)]
