8097486c1ad867da6a73ecfdf2d2c9552eb2528d
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-03-09 23:36:00 +0000

    dummy delete button added on all the itemlist classes

--- gui/list/itemlist.py
+++ gui/list/itemlist.py
@@ -2,12 +2,14 @@
 
 from PyQt4 import QtGui
 from PyQt4 import QtCore
+from core.localisation import _
 
 
 class itemList(QtGui.QTableWidget):
 
 	_app = None
 	_parent = None
+	_defaultColumns = (_('DELETE_COLUMN'),)
 
 	def __init__(self, parent, app):
 		"""
@@ -16,8 +18,8 @@ class itemList(QtGui.QTableWidget):
 		self._app = app
 		QtGui.QTableWidget.__init__(self, parent)
 		self._parent = parent
-		self.setColumnCount(len(self._columns))
-		self.setHorizontalHeaderLabels(self._columns)
+		self.setColumnCount(len(self._columns) + len(self._defaultColumns))
+		self.setHorizontalHeaderLabels(self._columns + self._defaultColumns)
 		self.verticalHeader().setVisible(False)
 		self.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
 		self.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
@@ -33,7 +35,9 @@ class itemList(QtGui.QTableWidget):
 		for index, row in enumerate(self.getData()):
 			if self.rowCount() < nbRowsToInsert:
 				self.insertRow(index)
-			self.insertItem(index, row)
+			itemsNumber = self.insertItem(index, row)
+			self.setCellWidget(index, itemsNumber, itemDeleteButton(self, index, _('DELETE_BUTTON')))
+
 		while self.rowCount() > nbRowsToInsert:
 			self.removeRow(self.rowCount() - 1)
 		self.resizeColumnsToContents()
@@ -63,3 +67,27 @@ class itemLocatorButton(QtGui.QPushButton):
 		self._table._parent.centerMapOnCoordinates(
 			self._table.getCoordinatesFromIndex(self._index)
 		)
+
+
+class itemDeleteButton(QtGui.QPushButton):
+	"""
+	QPushButton extended to connect the action _deleteItem when it is clicked.
+	"""
+	_table = None
+	_index = None
+
+	def __init__(self, table, index, *args):
+		"""
+		Construct
+		"""
+		QtGui.QItemDelegate.__init__(self, *args)
+		self._table = table
+		self._index = index
+		self.clicked.connect(self._deleteItem)
+
+	def _deleteItem(self):
+		"""
+		When a delete button is clicked, a confirmation dialog is displayed and
+		then the item is deleted
+		"""
+		print "delete"
